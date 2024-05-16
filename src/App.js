5e98bedba6526a42b522febf6916576c0ed2a0d7
jigneshchandegara <chandegarajignesh29@gmail.com>
2024-05-16 06:41:43 +0530

    exam

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Adminlogin from './componet/page/Adminlogin';
+import Reservationfrom from './componet/page/Reservationform';
+import Reservationlist from './componet/page/Reservationlist';
+import Navbar from './componet/header/Navbar';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { GET_ROOM_PENDING } from './componet/redux-saga/room/action';
+import { base_url, get_url } from './url';
+
+const getRole = () => {
+  const role = localStorage.getItem("role");
+  return role;
+};
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const role = getRole();
+  let dispatch = useDispatch();
+
+  useEffect(() => {
+    let url = base_url + get_url;
+    dispatch({ type: GET_ROOM_PENDING, payload: url })
+  })
+
+
+  if (!role || role === "") {
+    return (
+      <Routes>
+        <Route path="/" element={<Adminlogin />} />
+      </Routes>
+    );
+  }
+
+  if (role === "admin") {
+    return (
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/reservation" element={<Reservationfrom />} />
+          <Route path="/ReservationList" element={<Reservationlist />} />
+        </Routes>
+      </>
+    );
+  }
+
+
+
 }
 
 export default App;
